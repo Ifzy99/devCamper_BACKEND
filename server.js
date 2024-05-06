d6b10c7fcc6aa81d4df047b1aef4ca33d82b5f39
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-05-06 08:42:40 +0100

    added a route and controller to the project

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Routes files
+const bootcamps = require("./routes/bootcamps");
+
 //Load env variables
 dotenv.config({path: './config/config.env'})
 
 const app = express();
+
+//Mount routes 
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.get("/", (req, res)=>{
+    res.status(200).json({success: true})
+})
 
 const PORT = process.env.PORT;
 
