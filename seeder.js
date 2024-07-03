d0cb684b1192f2b4d6dbf6751a6680916a57cb50
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-07-03 19:20:56 +0100

    Created a model for Course and necessary code for seeder file

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load Bootcamp model
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -15,12 +16,17 @@ mongoose.connect(process.env.MONGO_URI);
 // Read the JSON files
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+
+);const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);    await Course.create(courses);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -32,7 +38,8 @@ const importData = async () => {
 // Delete Data
 const deleteData = async () => {
   try {
-    await Bootcamp.deleteMany();
+    await Bootcamp.deleteMany();    
+    await Course.deleteMany();
 
     console.log("Data Destroyed...".red.inverse);
     process.exit();
