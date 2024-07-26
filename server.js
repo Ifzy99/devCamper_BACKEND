9a2cce0fa6708e92f1742a07c91abcfbffa7f574
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-07-26 20:57:13 +0100

    updated work with xss protection  and cors

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 const connectDB = require("./config/db");
 
 //Load env variables
@@ -40,6 +44,22 @@ app.use(fileupload());
 
 //Sanitize data
 app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent xss attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100 // limit each IP to 100 requests per `window` (here, // 10 minutes)
+  });
+  app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
