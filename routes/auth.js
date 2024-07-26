7ec08d2f68acd21707aaf07a67beba8d307e5b50
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-07-26 13:59:43 +0100

    Work updated with new package installed

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require("../controller/auth");
+const { register, login,logout, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require("../controller/auth");
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const {  protect } = require("../middleware/auth")
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 router.get("/me", protect, getMe);
 router.put("/updatedetails", protect, updateDetails);
 router.put("/updatepassword", protect, updatePassword);
